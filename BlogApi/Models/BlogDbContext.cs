03702b629c17eb7fd6ac8222a19e07d061dd1696
FZoltan40 <farkasz@kkszki.hu>
2025-11-10 08:59:05 +0100

    DbContext is created.

---
+++ BlogApi/Models/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext()
+        {
+        }
+
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Blogger> bloggers { get; set; }
+    }
+}
